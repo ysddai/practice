3ec5b2c5e3759c7d50cb3f9e60db75452649a7c8
jen_yenpin <ysddai@gmail.com>
2016-08-09 07:59:56 +0800

    first

--- 29_单个表单项的检验/javascript.js
+++ 29_单个表单项的检验/javascript.js
@@ -1,42 +1,38 @@
-var msg = document.getElementById('msg'),
-    tip = document.getElementById('tip'),
-    btn = document.getElementById('btn'),
-
-    inputLength = 0;
+	var msg = document.getElementById('msg');
+	var btn = document.getElementById('btn');
+	var tip = document.getElementById('tip');
 
 
-    function validate(){
-        var str = msg.value;
-        if (getLength(str) == 0) {
-            tip.innerHTML = '姓名不能为空!';
-            tip.style.color = 'red';
-            msg.style.border = '2px solid red';
-        }else if (getLength(str) >= 4 && getLength(str) <= 16) {
-           tip.innerHTML = '格式正确!';
-           tip.style.color = 'lightgreen';
-           msg.style.border = '2px solid lightgreen'; 
-        }else{
-            tip.innerHTML = '字符数应为4~16位!';
-            tip.style.color = 'red';
-            msg.style.border = '2px solid red';
-        }
-    }
+	function validate(){
+		var str = msg.value;
+		if (getLength(str) == 0) {
+			msg.style.border = '2px solid red';
+			tip.style.color = 'red';
+			tip.innerHTML = '姓名不能为空';
+		}else if(getLength(str) >= 4 && getLength(str) <= 16 ){
+			msg.style.border = '2px solid lightGreen';
+			tip.style.color = 'lightGreen';
+			tip.innerHTML = '格式正确';
+		}else{
+			msg.style.border = '2px solid red';
+			tip.style.color = 'red';
+			tip.innerHTML = '字符数应为4~16位';
+		}
+	}
 
+	function getLength(str){
+	// var getLength = function(str){
+		var realLength = 0;
+		for (var i = 0; i < str.length; i++) {
+			charCode = str.charCodeAt(i);
+			if (charCode >= 0 && charCode <= 128) {
+				realLength += 1;
+			}else{
+				realLength += 2;
+			}
+		}
+			return realLength;
+	}
 
-   function getLength(str){
+	btn.onclick = validate;
 
-        for(var i = 0; i < str.length; i++){
-            charCode = str.charCodeAt(i);
-            if (charCode >= 0 && charCode <= 128) {
-                inputLength +=1;
-            }else{
-                inputLength +=2;
-            }
-        }
-            return inputLength;
-    };
-
-
-    btn.onclick = function(){
-        validate();
-    }
